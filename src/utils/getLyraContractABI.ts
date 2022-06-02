ba60033d73b8b4112717990579f27013a0fbad68
Joshua Kim <joshua0702@gmail.com>
2022-06-01 18:45:41 -0700

    set hardhat as peer and dev dependencies

--- src/utils/getLyraContractABI.ts
+++ src/utils/getLyraContractABI.ts
@@ -1,18 +1,7 @@
-import { ContractInterface } from '@ethersproject/contracts'
-import { getMarketDeploys, getGlobalDeploys } from '@lyrafinance/protocol'
-import { LyraContractId, LyraMarketContractId } from '../constants/contracts'
-// import LIQUIDITY_POOL_ABI from '../contracts/abis/LiquidityPool.json'
-// import LIQUIDITY_TOKENS_ABI from '../contracts/abis/LiquidityTokens.json'
-// import OPTION_GREEK_CACHE_ABI from '../contracts/abis/OptionGreekCache.json'
-// import OPTION_MARKET_ABI from '../contracts/abis/OptionMarket.json'
-// import OPTION_MARKET_PRICER_ABI from '../contracts/abis/OptionMarketPricer.json'
-// import OPTION_MARKET_VIEWER_ABI from '../contracts/abis/OptionMarketViewer.json'
-// import OPTION_MARKET_WRAPPER_ABI from '../contracts/abis/OptionMarketWrapper.json'
-// import OPTION_TOKEN_ABI from '../contracts/abis/OptionToken.json'
-// import POOL_HEDGER_ABI from '../contracts/abis/PoolHedger.json'
-// import SHORT_COLLATERAL_ABI from '../contracts/abis/ShortCollateral.json'
-// import SYNTHETIX_ADAPTER_ABI from '../contracts/abis/SynthetixAdapter.json'
-// import TEST_FAUCET_ABI from '../contracts/abis/TestFaucet.json'
+import { ContractInterface } from '@ethersproject/contracts';
+import { getGlobalDeploys,getMarketDeploys } from '@lyrafinance/protocol';
+
+import { LyraContractId, LyraMarketContractId } from '../constants/contracts';
 
 export default function getLyraContractABI(contractId: LyraContractId | LyraMarketContractId): ContractInterface {
   if (contractId == LyraContractId.OptionMarketViewer || 
